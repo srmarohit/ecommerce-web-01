cc57621d965ad20c69e2d24db83460f57d6c00b0
Rohit Sharma <srmarohit@gmail.com>
2021-01-21 13:57:20 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+// JavaScript source code
+$(document).ready(function () {
+    $('.food-slider').slick({
+        autoplay:true,
+        slidesToShow: 3,         // If you max no. of alides use to show then automatically pre and next btn disabled . no auto play.
+        slidesToScroll: 1,
+        prevArrow:'.prev-btn',
+        nextArrow: '.next-btn',
+
+        responsive: [{
+            breakpoint:992,
+            settings: {
+                slidesToShow:2
+            }
+        },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            }]
+    });
+
+/* Apply scaled class to nav trigger to show mobile nav menu. */
+
+    $('.nav-trigger').click(function () {
+        $('.site-content-wrapper').toggleClass('scaled');
+    });
+
+});
